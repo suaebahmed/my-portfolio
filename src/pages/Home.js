8add87c360ca29d6c0f71e07a03b70a0d32f3dcc
suaebahmed <suaebahmed12@gmail.com>
2023-12-09 06:32:01 +0600

    fixed padding

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,9 +26,9 @@ function Home() {
   return (
     <>
       <Navbar></Navbar>
-      <div className="max-w-screen-lg mx-auto pt-8">
+      <div className="max-w-screen-lg mx-auto">
         {/* container or hero section */}
-        <div className="w-full h-screen" id="home">
+        <div className="w-full h-screen pt-8" id="home">
           <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
             <p className="text-pink-600">Hi, my name is</p>
             <h1 className="text-4xl sm:text-3xl font-bold text-[#ccd6f6]">
